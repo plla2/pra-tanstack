c6a6e56346a558330b027712a7317e189d839625
plla2 <jhbljs92@naver.com>
2023-09-17 19:04:42 +0900

    ✨ Form Input들 컴포넌트 분리

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { useState } from "react";
 
 const PostForm = () => {
-  return <form action=""></form>;
+  const [post, setPost] = useState<{ [key: string]: string }>({
+    title: "",
+    body: "",
+  });
+  console.log(post);
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPost({ ...post, [e.target.name]: e.target.value });
+  };
+
+  const renderField = (label: string) => (
+    <div>
+      <label htmlFor="">{label}</label>
+      <input
+        onChange={handleChangeInput}
+        type="text"
+        name={label.toLowerCase()}
+        value={post[label.toLowerCase()]}
+      />
+    </div>
+  );
+
+  return (
+    <form>
+      {renderField("Title")}
+      {renderField("Body")}
+    </form>
+  );
 };
 
 export default PostForm;
